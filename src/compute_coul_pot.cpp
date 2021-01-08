6af0f917f7b01c3a6640fb1148e20618f56c3e30
robeme <robert.meissner@tuhh.de>
2021-01-08 22:23:57 +0100

    able to write out simple matrix with pair interactions

--- src/compute_coul_pot.cpp
+++ src/compute_coul_pot.cpp
@@ -31,6 +31,7 @@
 #include "kspace.h"
 #include "error.h"
 #include "comm.h"
+#include "memory.h"
 #include "domain.h"
 #include "math_const.h"
 
@@ -45,22 +46,23 @@ enum{OFF,INTER,INTRA};
 
 ComputeCoulPot::ComputeCoulPot(LAMMPS *lmp, int narg, char **arg) :
   Compute(lmp, narg, arg),
-  group2(nullptr)
+  group2(nullptr), gradQ_V(nullptr)
 {
-  if (narg < 4) error->all(FLERR,"Illegal compute group/group command");
-
-  scalar_flag = vector_flag = 1;
-  size_vector = 3;
-  extscalar = 1;
-  extvector = 1;
-
+  if (narg < 4) error->all(FLERR,"Illegal compute coul/pot command");
+  
+  array_flag = 1;
+  size_array_cols = 0;
+  size_array_rows = 0;
+  size_array_rows_variable = 0;
+  extarray = 0;
+  
   int n = strlen(arg[3]) + 1;
   group2 = new char[n];
   strcpy(group2,arg[3]);
 
   jgroup = group->find(group2);
   if (jgroup == -1)
-    error->all(FLERR,"Compute group/group group ID does not exist");
+    error->all(FLERR,"Compute coul/pot group ID does not exist");
   jgroupbit = group->bitmask[jgroup];
 
   pairflag = 1;
@@ -68,51 +70,53 @@ ComputeCoulPot::ComputeCoulPot(LAMMPS *lmp, int narg, char **arg) :
   boundaryflag = 1;
   molflag = OFF;
   matrixflag = 0;
+  
+  igroupnum = jgroupnum = natoms_original = natoms = 0;
 
   int iarg = 4;
   while (iarg < narg) {
     if (strcmp(arg[iarg],"pair") == 0) {
       if (iarg+2 > narg)
-        error->all(FLERR,"Illegal compute group/group command");
+        error->all(FLERR,"Illegal compute coul/pot command");
       if (strcmp(arg[iarg+1],"yes") == 0) pairflag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) pairflag = 0;
-      else error->all(FLERR,"Illegal compute group/group command");
+      else error->all(FLERR,"Illegal compute coul/pot command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"kspace") == 0) {
       if (iarg+2 > narg)
-        error->all(FLERR,"Illegal compute group/group command");
+        error->all(FLERR,"Illegal compute coul/pot command");
       if (strcmp(arg[iarg+1],"yes") == 0) kspaceflag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) kspaceflag = 0;
-      else error->all(FLERR,"Illegal compute group/group command");
+      else error->all(FLERR,"Illegal compute coul/pot command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"matrix") == 0) {
       if (iarg+2 > narg)
-        error->all(FLERR,"Illegal compute group/group command");
+        error->all(FLERR,"Illegal compute coul/pot command");
       if (strcmp(arg[iarg+1],"yes") == 0) matrixflag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) matrixflag = 0;
-      else error->all(FLERR,"Illegal compute group/group command");
+      else error->all(FLERR,"Illegal compute coul/pot command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"boundary") == 0) {
       if (iarg+2 > narg)
-        error->all(FLERR,"Illegal compute group/group command");
+        error->all(FLERR,"Illegal compute coul/pot command");
       if (strcmp(arg[iarg+1],"yes") == 0) boundaryflag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) boundaryflag  = 0;
-      else error->all(FLERR,"Illegal compute group/group command");
+      else error->all(FLERR,"Illegal compute coul/pot command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"molecule") == 0) {
       if (iarg+2 > narg)
-        error->all(FLERR,"Illegal compute group/group command");
+        error->all(FLERR,"Illegal compute coul/pot command");
       if (strcmp(arg[iarg+1],"off") == 0) molflag = OFF;
       else if (strcmp(arg[iarg+1],"inter") == 0) molflag = INTER;
       else if (strcmp(arg[iarg+1],"intra") == 0) molflag  = INTRA;
-      else error->all(FLERR,"Illegal compute group/group command");
+      else error->all(FLERR,"Illegal compute coul/pot command");
       if (molflag != OFF && atom->molecule_flag == 0)
-        error->all(FLERR,"Compute group/group molecule requires molecule IDs");
+        error->all(FLERR,"Compute coul/pot molecule requires molecule IDs");
       iarg += 2;
-    } else error->all(FLERR,"Illegal compute group/group command");
+    } else error->all(FLERR,"Illegal compute coul/pot command");
   }
 
-  vector = new double[size_vector];
+  gradQ_V = nullptr;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -120,7 +124,9 @@ ComputeCoulPot::ComputeCoulPot(LAMMPS *lmp, int narg, char **arg) :
 ComputeCoulPot::~ComputeCoulPot()
 {
   delete [] group2;
-  delete [] vector;
+  for (int i = 0; i < natoms; i++)
+    delete [] gradQ_V[i];
+  delete [] gradQ_V;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -130,18 +136,18 @@ void ComputeCoulPot::init()
   // if non-hybrid, then error if single_enable = 0
   // if hybrid, let hybrid determine if sub-style sets single_enable = 0
 
-  if (pairflag && force->pair == nullptr)
-    error->all(FLERR,"No pair style defined for compute group/group");
+  if (pairflag && force->pair == nullptr) 
+    error->all(FLERR,"No pair style defined for compute coul/pot");
   if (force->pair_match("^hybrid",0) == nullptr
       && force->pair->single_enable == 0)
-    error->all(FLERR,"Pair style does not support compute group/group");
+    error->all(FLERR,"Pair style does not support compute coul/pot");
 
-  // error if Kspace style does not compute group/group interactions
+  // error if Kspace style does not compute coul/pot interactions
 
   if (kspaceflag && force->kspace == nullptr)
-    error->all(FLERR,"No Kspace style defined for compute group/group");
+    error->all(FLERR,"No Kspace style defined for compute coul/pot");
   if (kspaceflag && force->kspace->group_group_enable == 0)
-    error->all(FLERR,"Kspace style does not support compute group/group");
+    error->all(FLERR,"Kspace style does not support compute coul/pot");
 
   if (pairflag) {
     pair = force->pair;
@@ -156,7 +162,7 @@ void ComputeCoulPot::init()
   if (kspaceflag) {
     kspace_correction();
     if ((fabs(e_correction) > SMALL) && (comm->me == 0))
-      error->warning(FLERR,"Both groups in compute group/group have a net charge; "
+      error->warning(FLERR,"Both groups in compute coul/pot have a net charge; "
                      "the Kspace boundary correction to energy will be non-zero");
   }
 
@@ -164,7 +170,7 @@ void ComputeCoulPot::init()
 
   jgroup = group->find(group2);
   if (jgroup == -1)
-    error->all(FLERR,"Compute group/group group ID does not exist");
+    error->all(FLERR,"Compute coul/pot group ID does not exist");
   jgroupbit = group->bitmask[jgroup];
 
   // need an occasional half neighbor list
@@ -175,6 +181,28 @@ void ComputeCoulPot::init()
     neighbor->requests[irequest]->compute = 1;
     neighbor->requests[irequest]->occasional = 1;
   }
+  
+  // get number of atoms in each group
+  
+  igroupnum = group->count(igroup);
+  jgroupnum = group->count(jgroup);
+  
+  natoms = atom->natoms; // TODO ... = igroupnum+jgroupnum;
+  
+  gradQ_V = new double*[natoms];
+  for (int i = 0; i < natoms; i++)
+  gradQ_V[i] = new double[natoms];
+  
+  natoms_original = natoms;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeCoulPot::setup()
+{
+  // one-time calculation of coulomb matrix
+
+  compute_array();
 }
 
 /* ---------------------------------------------------------------------- */
@@ -186,15 +214,49 @@ void ComputeCoulPot::init_list(int /*id*/, NeighList *ptr)
 
 /* ---------------------------------------------------------------------- */
 
-void ComputeCoulPot::compute_vector()
+void ComputeCoulPot::compute_array()
 {
-  invoked_scalar = invoked_vector = update->ntimestep;
-
-  scalar = 0.0;
-  vector[0] = vector[1] = vector[2] = 0.0;
+  if (natoms != natoms_original) reallocate();
+  
+  // initialize gradQ_V to zero  
+    
+  size_t nbytes = sizeof(double) * natoms;
+  if (nbytes)
+    for (int i = 0; i < natoms; i++)
+      memset(&gradQ_V[i][0],0,nbytes);
 
   if (pairflag) pair_contribution();
-  if (kspaceflag) kspace_contribution();
+  if (kspaceflag) kspace_contribution(); 
+  
+  FILE *fp = nullptr;
+  if (comm->me == 0) {
+    printf("*** trying to write out matrix ***\n");
+    fp = fopen("A.mat","w");
+    for (int i = 0; i < natoms; i++) {
+      for (int j = 0; j < natoms; j++) {
+        fprintf(fp, "%.3f ", gradQ_V[i][j]);
+      }
+      fprintf(fp,"\n");
+    }
+    if (fp && comm->me == 0) fclose(fp);
+  } 
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputeCoulPot::reallocate()
+{
+  // grow coulomb matrix
+  
+  for (int i = 0; i < natoms; i++)
+    delete [] gradQ_V[i];
+  delete [] gradQ_V;
+  
+  gradQ_V = new double*[natoms];
+  for (int i = 0; i < natoms; i++)
+    gradQ_V[i] = new double[natoms];
+
+  natoms_original = natoms;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -203,18 +265,28 @@ void ComputeCoulPot::pair_contribution()
 {
   int i,j,ii,jj,inum,jnum,itype,jtype;
   double xtmp,ytmp,ztmp,delx,dely,delz;
-  double rsq,eng,fpair,factor_coul,factor_lj;
+  double rsq,eng,fpair,factor_coul;
   int *ilist,*jlist,*numneigh,**firstneigh;
 
   double **x = atom->x;
   tagint *molecule = atom->molecule;
   int *type = atom->type;
   int *mask = atom->mask;
+  int *tag = atom->tag;
   int nlocal = atom->nlocal;
   double *special_coul = force->special_coul;
-  double *special_lj = force->special_lj;
-  int newton_pair = force->newton_pair;
 
+  double **gradQ_V_local = new double*[natoms];
+    for (int i = 0; i < natoms; i++)
+        gradQ_V_local[i] = new double[natoms];
+      
+  // initialize gradQ_V_local to zero      
+        
+  size_t nbytes = sizeof(double) * natoms;
+  if (nbytes)
+    for (int i = 0; i < natoms; i++)
+      memset(&gradQ_V_local[i][0],0,nbytes);
+  
   // invoke half neighbor list (will copy or build if necessary)
 
   neighbor->build_one(list);
@@ -226,9 +298,6 @@ void ComputeCoulPot::pair_contribution()
 
   // loop over neighbors of my atoms
   // skip if I,J are not in 2 groups
-
-  double one[4];
-  one[0] = one[1] = one[2] = one[3] = 0.0;
 
   for (ii = 0; ii < inum; ii++) {
     i = ilist[ii];
@@ -244,7 +313,6 @@ void ComputeCoulPot::pair_contribution()
 
     for (jj = 0; jj < jnum; jj++) {
       j = jlist[jj];
-      factor_lj = special_lj[sbmask(j)];
       factor_coul = special_coul[sbmask(j)];
       j &= NEIGHMASK;
 
@@ -277,43 +345,19 @@ void ComputeCoulPot::pair_contribution()
       jtype = type[j];
 
       if (rsq < cutsq[itype][jtype]) {
-        eng = pair->single(i,j,itype,jtype,rsq,factor_coul,factor_lj,fpair);
-
-        // energy only computed once so tally full amount
-        // force tally is jgroup acting on igroup
-
-        if (newton_pair || j < nlocal) {
-          one[0] += eng;
-          if (ij_flag) {
-            one[1] += delx*fpair;
-            one[2] += dely*fpair;
-            one[3] += delz*fpair;
-          }
-          if (ji_flag) {
-            one[1] -= delx*fpair;
-            one[2] -= dely*fpair;
-            one[3] -= delz*fpair;
-          }
-
-        // energy computed twice so tally half amount
-        // only tally force if I own igroup atom
-
-        } else {
-          one[0] += 0.5*eng;
-          if (ij_flag) {
-            one[1] += delx*fpair;
-            one[2] += dely*fpair;
-            one[3] += delz*fpair;
-          }
-        }
+         gradQ_V_local[tag[i]-1][tag[j]-1] += pair->single(i,j,itype,jtype,rsq,factor_coul,0.0,fpair);
       }
     }
   }
+  
+  // need to gather data from all procs
 
-  double all[4];
-  MPI_Allreduce(one,all,4,MPI_DOUBLE,MPI_SUM,world);
-  scalar += all[0];
-  vector[0] += all[1]; vector[1] += all[2]; vector[2] += all[3];
+  for (int i = 0; i < natoms; i++)
+    MPI_Reduce(gradQ_V_local[i],gradQ_V[i],natoms,MPI_DOUBLE,MPI_SUM,0,world);
+    
+  for (int i = 0; i < natoms; i++)
+    delete [] gradQ_V_local[i];
+  delete [] gradQ_V_local;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -323,21 +367,21 @@ void ComputeCoulPot::kspace_contribution()
   double *vector_kspace = force->kspace->f2group;
 
   force->kspace->compute_group_group(groupbit,jgroupbit,0);
-  scalar += 2.0*force->kspace->e2group;
-  vector[0] += vector_kspace[0];
-  vector[1] += vector_kspace[1];
-  vector[2] += vector_kspace[2];
+  //scalar += 2.0*force->kspace->e2group;
+  //vector[0] += vector_kspace[0];
+  //vector[1] += vector_kspace[1];
+  //vector[2] += vector_kspace[2];
 
   // subtract extra A <--> A Kspace interaction so energy matches
   //   real-space style of compute group-group
   // add extra Kspace term to energy
 
   force->kspace->compute_group_group(groupbit,jgroupbit,1);
-  scalar -= force->kspace->e2group;
+  //scalar -= force->kspace->e2group;
 
   // self energy correction term
 
-  scalar -= e_self;
+  //scalar -= e_self;
 
   // k=0 boundary correction term
 
@@ -350,7 +394,7 @@ void ComputeCoulPot::kspace_contribution()
     // 3d Ewald just uses zprd since slab_volfactor = 1.0
 
     double volume = xprd*yprd*zprd*force->kspace->slab_volfactor;
-    scalar -= e_correction/volume;
+    //scalar -= e_correction/volume;
   }
 }
 
