24404b9a2c7bc6d9660ec176bbe464d48c65a9b2
robeme <robert.meissner@tuhh.de>
2021-01-18 15:20:43 +0100

    forgot to rechange header

--- src/USER-CONP/compute_coul_matrix.h
+++ src/USER-CONP/compute_coul_matrix.h
@@ -39,7 +39,7 @@ class ComputeCoulMatrix : public Compute {
  private:
   char *group2;
   int jgroup,jgroupbit,othergroupbit;
-  int jgroupnum, igroupnum, ngroup;
+  bigint jgroupnum, igroupnum, ngroup;
   int recalc_every;
   double **cutsq,**gradQ_V;
   double g_ewald,eta;
